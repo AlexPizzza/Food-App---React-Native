7f254b115fc35ad3b1b9ae611ac313afe7b53224
Alex Pita <pizzaalex99@gmail.com>
2021-09-21 19:23:30 +0300

    Removed API key

--- src/api/yelp.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "https://api.yelp.com/v3/businesses",
-    headers: {
-        Authorization: "Bearer z1h96Ivn20ycpm6R398wbA3u_iX8TfYRpE36XH_IQaTWyquxD7QpI2Gm3j5IWls3JcVy0o1fymdNLEBbUjJJmfclL4MkDlbbGGRU-FGtFyUurSN6IyDffCM2aulMYHYx"
-    }
-});
